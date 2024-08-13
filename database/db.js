8999c8eb9ebcb34a485817e7c4edc9cca6728c7b
rishashetty05 <rishashetty05@gmail.com>
2024-08-13 23:25:30 +0530

    Updated DB with Mongoose parameters

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Connection = () => {
     const URL = '';
     try {
-        mongoose.connect(URL, { } )
+        mongoose.connect(URL, { useUnifiedTopology : true, useNewUrlParser : true } );
 
     } catch(error){
         console.log("Error while connecting to database ", error);
