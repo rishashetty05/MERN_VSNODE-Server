15baeb883c2cc7c40be9067109f21553a3956ae9
rishashetty05 <rishashetty05@gmail.com>
2024-08-15 10:44:09 +0530

    Hiding credentials in URL

--- database/db.js
+++ database/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const Connection = async () => {
-    const URL = `mongodb+srv://rishashetty05:code4interview@crud-app.mbyqfps.mongodb.net/?retryWrites=true&w=majority&appName=crud-app`;
+const Connection = async ( username, password ) => {
+//const Connection = async () => {
+    const URL = `mongodb+srv://${username}:${password}@crud-app.mbyqfps.mongodb.net/?retryWrites=true&w=majority&appName=crud-app`;
+    //const URL = `mongodb+srv://rishashetty05:code4interview@crud-app.mbyqfps.mongodb.net/?retryWrites=true&w=majority&appName=crud-app`;
     try {
         await mongoose.connect(URL, { useUnifiedTopology : true, useNewUrlParser : true } ); //try mongoose.connect(URL) next time
         console.log("Database connected successfully");
     } catch(error){
         console.log("Error while connecting to database ", error);
-    }
+    } 
 }
 
 export default Connection;
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
 import Connection from './database/db.js';
 
 const app = express();
 
+dotenv.config(); //INITILIZE
+
 const PORT = 8000;
 
-Connection();
+const username = process.env.DB_USERNAME; //to update credentials hiding from connect string URL and ensure security
+const password = process.env.DB_PASSWORD;
 
+Connection( username, password );
+
+//Connection();
 app.listen(PORT, () => console.log(`Server is running successfully on port ${PORT}`));
