7c2a1406f48d04ef8820ea231d25030a3135b137
rishashetty05 <rishashetty05@gmail.com>
2024-08-15 11:46:55 +0530

    Server router running on 8000 & Cors issue resolved between AddUser & route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import Connection from './database/db.js';
+import Routes from './router/route.js';
 
 const app = express();
 
 dotenv.config(); //INITILIZE
+
+app.use(cors());
+
+app.use('/', Routes); //no default end point so blank
 
 const PORT = 8000;
 
