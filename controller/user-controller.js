beb4aeafe2ecb89a1f302b73c629c07287c327f5
rishashetty05 <rishashetty05@gmail.com>
2024-08-15 12:55:12 +0530

    Addinng body-parser to handle post api sent from FE

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,3 +1,9 @@
-export const addUser = () => {
-    console.log("hello")
+
+
+export const addUser = (request, response) => {
+    //console.log("hello")
+    const user = request.body;
+    //but node.js does not handle post api body sent from front end whihc can be solved by body-parser
+    
+    console.log(user)
 }
