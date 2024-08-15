b025c9eb41bb6d9b9c89818ee93d3eef9fb787e3
rishashetty05 <rishashetty05@gmail.com>
2024-08-15 13:03:23 +0530

    body-parser phase 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 import Connection from './database/db.js';
 import Routes from './router/route.js';
@@ -8,6 +9,9 @@ import Routes from './router/route.js';
 const app = express();
 
 dotenv.config(); //INITILIZE
+
+ app.use(bodyParser.json({ extended: true }));//to handle post api body content sent form port 3000
+ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
