9526b2c36f5da11c48b0cd9f15251292e02ac5f7
rishashetty05 <rishashetty05@gmail.com>
2024-08-15 13:05:48 +0530

    Missed edits

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -4,6 +4,6 @@ export const addUser = (request, response) => {
     //console.log("hello")
     const user = request.body;
     //but node.js does not handle post api body sent from front end whihc can be solved by body-parser
-    
-    console.log(user)
+
+    console.log(user);
 }
