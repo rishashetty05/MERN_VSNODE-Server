524c789b76d47b5cb619748b2fa0edf177a4a05f
rishashetty05 <rishashetty05@gmail.com>
2024-09-09 22:25:44 +0530

    mthd 1 to get params => {id}

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -28,7 +28,7 @@ export const getUsers =  async (request, response) => {
 export const getUser = async (request, response) => {
         console.log(request.params.id);
     try {
-        const user = await User.find({});
+        const user = await User.find({userId: request.params.id});
         //const user = await User.findById({userId: request.params.id});
         response.status(200).json(user);
     } catch (error) {
